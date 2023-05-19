2bb97580745961c2eba78864d9cba76863b02078
waihsu <hsuw647@gmail.com>
2023-05-19 17:20:43 +0630

    not login go to log in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 import BlogPost from "@/components/BlogPost";
 import Post from "./posts/page";
-import { useRouter } from "next/navigation";
-import { useContext } from "react";
-import { AuthContext } from "@/context/AuthContext";
+// import { useRouter } from "next/navigation";
+// import { useContext } from "react";
+// import { AuthContext } from "@/context/AuthContext";
 
 export default function App() {
-  const { token } = useContext(AuthContext);
-  // console.log(token);
-  if (!token) {
-    useRouter().push("/login");
-  }
+  // const { token } = useContext(AuthContext);
+  // // console.log(token);
+  // if (!token) {
+  //   useRouter().push("/login");
+  // }
   return (
     <div>
       <BlogPost />
